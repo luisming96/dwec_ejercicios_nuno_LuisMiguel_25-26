83a8af0564c5e5b3dcbde1699d2c0a3cf4e538dc
luisming96 <luisming96@gmail.com>
2025-09-12 13:56:32 +0200

    arreglos

--- VisualStudio/dwec_t02_Nuno_LuisMiguel/dwec_T02_Nuno_LuisMiguel_Ej1/js/ejercicio02.js
+++ VisualStudio/dwec_t02_Nuno_LuisMiguel/dwec_T02_Nuno_LuisMiguel_Ej1/js/ejercicio02.js
@@ -1,21 +1,21 @@
 console.log("T02 - Ejercicio 02");
 let numero = parseInt(prompt("Introduzca un numero para comprobar si es multiplo de 2, 5, ambos o ninguno"));
-if(isNaN(numero)) {   
+
+if(isNaN(numero)){   
 console.log("Error: No ha introducido un número válido");
 alert("Error: Por favor, introduzca un número válido.");
-
 } else {
-if (numero % 2 === 0 && numero % 5 === 0) {
+    if (numero % 2 === 0 && numero % 5 === 0) {
     console.log("El número introducido es multiplo de 2 y 5");
     alert("El número " + numero + " es múltiplo de 2 y de 5.");
-} else if (numero % 2 === 0) {
+    } else if (numero % 2 === 0) {
     console.log("El número introducido es multiplo de 2");
     alert("El número " + numero + " es múltiplo de 2.");
-} else if (numero % 5 === 0) {
+    } else if (numero % 5 === 0) {
     console.log("El número introducido es multiplo de 5");
     alert("El número " + numero + " es múltiplo de 5.");
-} else {
+    } else {
     console.log("El número introducido no es multiplo de 2 ni de 5");
     alert("El número " + numero + " no es múltiplo de 2 ni de 5.");
+    } 
 }
-    }
--- VisualStudio/dwec_t02_Nuno_LuisMiguel/dwec_T02_Nuno_LuisMiguel_Ej1/js/ejercicio03.js
+++ VisualStudio/dwec_t02_Nuno_LuisMiguel/dwec_T02_Nuno_LuisMiguel_Ej1/js/ejercicio03.js
@@ -1,3 +1,22 @@
 console.log("T02 - Ejercicio 03");
-let currentYear = new Date().getFullYear();
-let continuar = true;
+let repetir = "s";
+
+while (repetir === "s") {
+let actual = new Date().getFullYear();
+let entrada = prompt("Introduce un año entre 0 y " + actual + ":");
+let anio = Number(entrada);
+
+if (anio >= 0 && anio <= actual) {
+    if ((anio % 4 === 0 && anio % 100 !== 0) || (anio % 400 === 0)) {
+    alert(anio + " es bisiesto");
+    console.log(anio + " es bisiesto");
+    } else {
+    alert(anio + " no es bisiesto");
+    console.log(anio + " no es bisiesto");
+    }
+} else {
+    alert("El año debe estar entre 0 y " + actual);
+}
+
+repetir = prompt("¿Quieres comprobar otro año? (s/n)").toLowerCase();
+}
